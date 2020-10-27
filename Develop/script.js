d035f10245c04d7c5e7fe635b2b4a7d7f06de0cd
Mike Bannoura <mikebannoura@gmail.com>
2020-10-26 21:25:54 -0600

    Added icon img

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,52 @@
 // display current Date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //Declare Variables
-<<<<<<< HEAD
-//  var x = document.createElement('img');
-//    x.setAttribute('src', '/Assets/images/save.png');
-//    x.setAttribute("width", "50");
-//    x.setAttribute("height", "50");
-//    document.body.appendChild(x);
-=======
->>>>>>> 998c56295b920e704bb22376ebf35becb89402ec
+ var x = document.createElement('img');
+   x.setAttribute('src', '../Assets/images/save.png');
+   x.setAttribute("width", "50");
+   x.setAttribute("height", "50");
+   
 
 var schedule = [
    {
       time: "09:00 AM",
       des: "",
-<<<<<<< HEAD
-      bt: "save",
-=======
-      bt: "Save",
->>>>>>> 998c56295b920e704bb22376ebf35becb89402ec
    },
    {
       time: "10:00 AM",
       des: "",
-      bt: "save",
    },
    {
       time: "11:00 AM",
       des: "",
-      bt: "save",
    },
    {
       time: "12:00 PM",
       des: "",
-      bt: "save",
    },
    {
       time: "13:00 PM",
       des: "",
-      bt: "save",
    },
    {
       time: "14:00 PM",
       des: "",
-      bt: "save",
    },
    {
       time: "15:00 PM",
       des: "",
-      bt: "save",
    },
    {
       time: "16:00 PM",
       des: "",
-      bt: "save",
    },
    {
       time: "17:00 PM",
       des: "",
-      bt: "save",
    }
 ];
 // Declare vars
 var currentHour = parseInt(moment().format("HH:mm"));
-console.log(currentHour)
 // for loop for schedule array objects.
 $.each(schedule, function (key, value) {
  
@@ -73,23 +56,30 @@ $.each(schedule, function (key, value) {
    // console.log("block hour", temp)
    var newRow = $("<div>")
    newRow.addClass("row");
+   
    //if else statement to check if the it is in past, present or future. textarea will change color accordingly
    if (temp < currentHour) {
       newRow.append("<div class='hour col-md-2'>" + value.time + "</div>" + "<br/>");
       newRow.append("<textarea class='past col-md-8'>" + value.des + "</textarea>");
-      newRow.append("<div class='saveBtn col-md-2'>" + value.bt + "</div><br/>");
+      var imgDiv = $("<div class='saveBtn col-md-2'></div><br/>");
+      imgDiv.append(x);
+      newRow.append(imgDiv);
       $(".container").append(newRow);
    }
    else if (temp === currentHour) {
       newRow.append("<div class='hour col-md-2'>" + value.time + "</div>" + "<br/>");
       newRow.append("<textarea class='present col-md-8'>" + value.des + "</textarea>");
-      newRow.append("<div class='saveBtn col-md-2'>" + value.bt + "</div><br/>");
+      var imgDiv = $("<div class='saveBtn col-md-2'></div><br/>");
+      imgDiv.append(x);
+      newRow.append(imgDiv);
       $(".container").append(newRow);
    }
    else {
       newRow.append("<div class='hour col-md-2'>" + value.time + "</div>" + "<br/>");
       newRow.append("<textarea class='future col-md-8'>" + value.des + "</textarea>");
-      newRow.append("<div class='saveBtn col-md-2'>" + value.bt + "</div><br/>");
+      var imgDiv = $("<div class='saveBtn col-md-2'></div><br/>");
+      imgDiv.append(x);
+      newRow.append(imgDiv);
       $(".container").append(newRow);
    }
 });
@@ -105,7 +95,6 @@ $(".row").each(function () {
 //On click on the Save button set value in local Storage with timestamp and Description for it. 
 $(".saveBtn").on("click", function (time, value) {
    var time = $(this).siblings(".hour").text();
-   //  console.log(time)
    var des = $(this).siblings("textarea").val();
    localStorage.setItem(time, des);
 });
